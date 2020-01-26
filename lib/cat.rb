319bb912423925a5aeac8e340eddae9e83615148
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-01-26 20:25:43 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,25 @@
+require 'pry'
 class Cat
   # code goes here
+  attr_reader :name
+  attr_accessor :mood, :owner
+  
+  @@all = []
+
+  def initialize(name)
+   # binding.pry
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+  
+   def self.all
+    @@all 
+  end 
+  def feed_cats
+    @@pets[:cats].each do |cat|
+    cat.mood = 'happy'
+    end
+  end
 end
